2175a15b3f370a0554e0ed38ca976c062b64ff7b
ademuliadi <ade.muliadi1@gmail.com>
2025-04-14 22:55:30 +0800

    melakuakn konfigurasi database

---
+++ config/database.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := "root:@tcp(127.0.0.1:3306)/db_golang?charset=utf8mb4&parseTime=True&loc=Local"
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Gagal koneksi ke database!")
+	}
+	DB = database
+	fmt.Println("Berhasil koneksi ke database")
+}
